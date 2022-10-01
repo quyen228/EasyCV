13cd1ab0a0733b1ec80317fe399996e9253f668b
quyen228 <69098210+quyen228@users.noreply.github.com>
2022-10-01 18:23:25 +0700

    Update constant.py

--- easycv/utils/constant.py
+++ easycv/utils/constant.py
@@ -1,6 +1,6 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 import os
 
-CACHE_DIR = os.path.expanduser('~/.cache/easycv/')
+CACHE_DIR = os.path.expanduser('./easycv/')
 
 MAX_READ_IMAGE_TRY_TIMES = 10
